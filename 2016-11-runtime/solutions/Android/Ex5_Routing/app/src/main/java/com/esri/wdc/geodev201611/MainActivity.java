d254a24615e33f250f56eaa22fe8d13821b7be56
Gary Sheppard <gsheppard@esri.com>
2016-10-26 11:52:18 -0400

    Android: added detailed comment on authentication (#36)

--- 2016-11-runtime/solutions/Android/Ex5_Routing/app/src/main/java/com/esri/wdc/geodev201611/MainActivity.java
+++ 2016-11-runtime/solutions/Android/Ex5_Routing/app/src/main/java/com/esri/wdc/geodev201611/MainActivity.java
@@ -114,6 +114,14 @@ public class MainActivity extends Activity {
 
         // Exercise 5: Create routing objects
         routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+        /**
+         * Note: for ArcGIS Online routing, this tutorial uses a username and password
+         * in the source code for simplicity. For security reasons, you would not
+         * do it this way in a real app. Instead, you would do one of the following:
+         * - Use an OAuth 2.0 user login
+         * - Use an OAuth 2.0 app login (not directly supported in ArcGIS Runtime Quartz as of Beta 2)
+         * - Challenge the user for credentials
+         */
         // Don't share this code without removing plain text username and password!!!
         routeTask.setCredential(new UserCredential("theUsername", "thePassword"));
         try {
